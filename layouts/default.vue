8deafd3bd5b4a7161232c8dfd3c70c3977ad86f6
cooldev23 <52139107+cooldev23@users.noreply.github.com>
2024-05-14 16:00:33 -0600

    Update layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,16 @@
 <template>
-  <div class="container mx-auto max-w-4xl">
+  <div class="container">
     <header class="flex justify-between items-center mt-4">
-      <div>
-        <NuxtLink to="/" class="text-2xl p-2 font-semibold hover:bg-blue-200">Sean Cordill</NuxtLink>
-      </div>
-      <div class="flex items-center">
-        <Menu />
-        <ClientOnly>
-          <ColorModeSelector />
-        </ClientOnly>
+      <div class="mx-auto">
+        <div>
+          <NuxtLink to="/" class="text-2xl p-2 font-semibold hover:bg-blue-200">Rosebud Wellness</NuxtLink>
+        </div>
+        <div class="flex items-center">
+          <Menu />
+          <ClientOnly>
+            <ColorModeSelector />
+          </ClientOnly>
+        </div>
       </div>
     </header>
     <main class="mt-10 p-2">
@@ -19,7 +21,7 @@
 
 <script setup>
 useHead({
-  titleTemplate: '%s - Sean Cordill',
+  titleTemplate: '%s - Rosebud Wellness',
   link: [
     {
       rel: 'preconnect',
@@ -42,5 +44,9 @@ body {
 
 body {
   @apply bg-gray-100 dark:bg-gray-900 text-gray-700 dark:text-gray-300
+}
+
+header {
+  background-image: url('~/public/img/header-image.jpg');
 }
 </style>
